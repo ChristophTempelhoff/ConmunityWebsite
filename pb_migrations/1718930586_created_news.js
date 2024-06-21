a062683796b7653da2244dafac5a5061a46b8a49
Christoph Tempelhoff <Christoph.Tempelhoff@gmail.com>
2024-06-21 02:46:07 +0200

    Backend fix #1 (Resetted DB)

--- pb_migrations/1718930586_created_news.js
+++ pb_migrations/1718930586_created_news.js
@@ -1,21 +1,16 @@
 /// <reference path="../pb_data/types.d.ts" />
 migrate((db) => {
-  const dao = new Dao(db);
-  const collection = dao.findCollectionByNameOrId("pdx56t4u6eqn378");
-
-  return dao.deleteCollection(collection);
-}, (db) => {
   const collection = new Collection({
-    "id": "pdx56t4u6eqn378",
-    "created": "2024-06-19 12:28:53.456Z",
-    "updated": "2024-06-20 23:40:23.635Z",
-    "name": "News",
+    "id": "cdk9x2zjhkazh7y",
+    "created": "2024-06-21 00:43:06.856Z",
+    "updated": "2024-06-21 00:43:06.856Z",
+    "name": "news",
     "type": "base",
     "system": false,
     "schema": [
       {
         "system": false,
-        "id": "vise4nk9",
+        "id": "raz70n7a",
         "name": "title",
         "type": "text",
         "required": false,
@@ -29,19 +24,21 @@ migrate((db) => {
       },
       {
         "system": false,
-        "id": "uio055yq",
+        "id": "nvuncbm6",
         "name": "desc",
-        "type": "editor",
-        "required": true,
+        "type": "text",
+        "required": false,
         "presentable": false,
         "unique": false,
         "options": {
-          "convertUrls": false
+          "min": null,
+          "max": null,
+          "pattern": ""
         }
       },
       {
         "system": false,
-        "id": "awr9y735",
+        "id": "qy0nictf",
         "name": "img",
         "type": "file",
         "required": false,
@@ -66,4 +63,9 @@ migrate((db) => {
   });
 
   return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("cdk9x2zjhkazh7y");
+
+  return dao.deleteCollection(collection);
 })
