cb4e6aea29a114f752178c14b6e4d989fc19302b
Christoph Tempelhoff <Christoph.Tempelhoff@gmail.com>
2024-06-21 22:54:50 +0200

    Added safety feature for news, so we always have news displayable

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,6 +1,8 @@
+import { NewInterface } from './../ClassesAndInterfaces/New';
 import { Injectable } from '@angular/core';
-import { NewInterface } from "../ClassesAndInterfaces/New";
 import PocketBase, { RecordModel } from 'pocketbase';
+
+declare var require: any
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class NewsService {
 
   constructor() { }
 
-  async GetNews(): Promise<RecordModel[]>{
+  async GetNewsFromDB(): Promise<RecordModel[]>{
     const pb = new PocketBase('http://127.0.0.1:8090');
     var news: NewInterface[] = [];
     
@@ -18,5 +20,18 @@ export class NewsService {
     });
     console.log(records);
     return records;
+  }
+
+  getNewsFromJSON(): NewInterface[]{
+    const results = require('../ClassesAndInterfaces/news.json').items;
+    var news: NewInterface[] = results.map((record: { [x: string]: any; }) => ({
+      id: record['id'],
+      title: record['title'],
+      desc: record['desc'],
+      date: new Date(record['date']),
+      url: record['url']
+    }));
+
+    return news;
   }
 }
