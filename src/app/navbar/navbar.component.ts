388b15f2a6994674df5a1adbe4ed15fe3fd37087
KingA2043 <froscilein12@outlook.de>
2024-06-23 22:03:48 +0200

    Include Font, animations and finished About-Us

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private elementRef: ElementRef) { }
+
+
+  collapseNavbar(): void {
+
+    let navbar = this.elementRef.nativeElement.querySelector('.navbar-collapse');
+
+    navbar.classList.remove('show');
+
+  }
 }
+
+
