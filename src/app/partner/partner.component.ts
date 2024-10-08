a46c94b51e57588b0e32d5e00f181342c25156ea
Christoph Tempelhoff <c.tempelhoff@perschmann.de>
2024-10-08 22:17:40 +0200

    Added functionality, styling needs to be improved @KingA2043

--- src/app/partner/partner.component.ts
+++ src/app/partner/partner.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PartnersService } from '../services/partners/partners.service';
+import { Partner } from '../ClassesAndInterfaces/Partner';
 
 @Component({
   selector: 'app-partner',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./partner.component.css']
 })
 export class PartnerComponent {
+PartnersTmp: Partner[] = [];
+Partners: Partner[][] = [];
+rowsToIterate: number[] = [];
+  constructor(){
+    this.initializePartners();
+  }
 
+  async initializePartners(){
+    var ps: PartnersService = new PartnersService();
+    this.PartnersTmp = await ps.getPartnersFromDB();
+    var rows: number = Math.ceil(this.PartnersTmp.length / 3);
+    
+    for (let i = 0; i < rows; i++) {
+      var partnersArr: Partner[] = [];
+      partnersArr.push(this.PartnersTmp[0]);
+      partnersArr.push(this.PartnersTmp[1]);
+      partnersArr.push(this.PartnersTmp[2]);
+      console.log("PartnersArr: ");
+      console.log(partnersArr);
+      
+      
+
+      this.Partners.push(partnersArr);
+      console.log("Partners: ");
+      console.log(this.Partners[i]);
+      this.PartnersTmp.splice(0,3);
+      this.rowsToIterate.push(i);
+    }
+    console.log(this.rowsToIterate);
+    
+  }
 }
