0ec0ce22a3d5a4b65a6517997235b69230dec3bf
KingA2043 <froscilein12@outlook.de>
2024-10-12 19:32:09 +0200

    Desiign Partner und Animation

--- src/app/partner/partner.component.ts
+++ src/app/partner/partner.component.ts
@@ -5,7 +5,7 @@ import { Partner } from '../ClassesAndInterfaces/Partner';
 @Component({
   selector: 'app-partner',
   templateUrl: './partner.component.html',
-  styleUrls: ['./partner.component.css']
+  styleUrls: ['./partner.component.css', './sponsors.css']
 })
 export class PartnerComponent {
 PartnersTmp: Partner[] = [];
@@ -25,18 +25,15 @@ rowsToIterate: number[] = [];
       partnersArr.push(this.PartnersTmp[0]);
       partnersArr.push(this.PartnersTmp[1]);
       partnersArr.push(this.PartnersTmp[2]);
-      console.log("PartnersArr: ");
-      console.log(partnersArr);
-      
+    
       
 
       this.Partners.push(partnersArr);
-      console.log("Partners: ");
-      console.log(this.Partners[i]);
+     
       this.PartnersTmp.splice(0,3);
       this.rowsToIterate.push(i);
     }
-    console.log(this.rowsToIterate);
+
     
   }
 }
