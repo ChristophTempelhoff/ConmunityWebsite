cbba3dff76d623734f75590249999e17f9f88c6d
KingA2043 <froscilein12@outlook.de>
2025-04-10 17:04:11 +0200

    formatting

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { News } from './../ClassesAndInterfaces/New';
 import { Injectable } from '@angular/core';
-import PocketBase, { RecordModel } from 'pocketbase';
-import { catchError, Observable, throwError } from 'rxjs';
+import {  Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +18,5 @@ export class NewsService {
 
   getSingleNewsFromDB(id: string): Observable<News>{
     return this.http.get<News>(this.baseURL + "/news/singlenews.php/" + id);
-    // let records = await this.pb.collection('News').getOne(id);
-    // return {id: records['id'], title: records['title'], desc: records['desc'], introduction: records['introduction'], date: new Date(records['created']), ImageUrl: this.pb.files.getUrl(records, records['img'])};
   }
 }
